f237f295ba20fa9aa820b8628e4998f08eab08cc
Joshua Crum <joshua.crum@gmail.com>
2019-05-09 19:26:49 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -26,3 +26,25 @@ def word_substituter(tweet)
   end
   words.join(" ")
 end
+
+def bulk_tweet_shortener(tweets_array)
+  tweets_array.map do |tweet|
+    puts word_substituter(tweet)
+  end
+end 
+
+def selective_tweet_shortener(tweet)
+  if tweet.chars.length > 140  
+    word_substituter(tweet)
+  else 
+    tweet 
+  end 
+end 
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).chars.length > 140  
+    truncated = word_substituter(tweet).chars[0..136].push("...").join("")
+  else 
+    word_substituter(tweet) 
+  end 
+end 
